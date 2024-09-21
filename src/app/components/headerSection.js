29e0a764cea2391296c9cc63a8f43ae13e86851c
tarasshulhan <taras.shulhan@gmail.com>
2024-09-21 01:29:17 -0400

    test

--- src/app/components/headerSection.js
+++ src/app/components/headerSection.js
@@ -9,9 +9,10 @@ const HeaderSection = () => {
         backgroundImage: `url(${image})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
+
         backgroundRepeat: "no-repeat",
         backgroundAttachment: "fixed",
-        height: "100vh",
+        // height: "100vh",
         margin: "0",
         width: "100%",
         borderRadius: "0",
